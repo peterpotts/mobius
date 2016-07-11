63adfbbc9473d9c415fe1ea51c4a0f84391c973f
Peter Potts <peter.potts@cointrust.com>
2016-07-10 22:54:44 -0700

    Add Pi implementation.

--- src/main/scala/com/peterpotts/mobius/Application.scala
+++ src/main/scala/com/peterpotts/mobius/Application.scala
@@ -8,6 +8,7 @@ object Application extends LazyLogging {
   def main(args: Array[String]): Unit = {
     fixedArithmetic()
     variableArithmetic()
+    pi()
   }
 
   def fixedArithmetic(): Unit = {
@@ -36,6 +37,34 @@ object Application extends LazyLogging {
     println("Sqrt 4 - Sqrt 2 = " + new VariableTensorDigitizer(Tensor.minus, sqrt4, sqrt2).precision(100).min.rational)
     println("Sqrt 4 * Sqrt 2 = " + new VariableTensorDigitizer(Tensor.times, sqrt4, sqrt2).precision(100).min.rational)
     println("Sqrt 4 / Sqrt 2 = " + new VariableTensorDigitizer(Tensor.divide, sqrt4, sqrt2).precision(100).min.rational)
+  }
+
+  def pi(): Unit = {
+    def oneTo(n: BigInt): Matrix = {
+      val guess = BigInt(0).setBit(n.bitLength / 2)
+      Matrix(Vector(guess, 1), Vector(n, guess))
+    }
+
+    def sqrt(n: BigInt): Digitizer = new VariableMatrixDigitizer(oneTo(n), sqrt(n))
+
+    def divide(x: Digitizer, y: Digitizer) = new VariableTensorDigitizer(Tensor.divide, x, y)
+
+    val q0 = Matrix(Vector(6795705, 213440), Vector(6795704, 213440))
+
+    def q(n: BigInt) = {
+      val cn = (2 * n - 1) * (6 * n - 5) * (6 * n - 1) * (545140134 * n + 13591409)
+      val dn = (2 * n - 1) * (6 * n - 5) * (6 * n - 1) * (n + 1)
+      val en = BigInt("10939058860032000") * n * n * n * n
+      Matrix(Vector(en - dn - cn, en + dn + cn), Vector(en + dn - cn, en - dn + cn))
+    }
+
+    def sq(n: BigInt): Digitizer = new FixedMatrixDigitizer(q(n), sq(n + 1))
+
+    val phi = new FixedMatrixDigitizer(q0, sq(1))
+    val pi = divide(sqrt(10005), phi)
+
+    println("Sqrt 10005 = " + sqrt(10005).precision(100).min.rational)
+    println("Pi = " + pi.precision(100).min.rational)
   }
 
   def main2(args: Array[String]): Unit = {
